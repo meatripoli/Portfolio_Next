8f003d1a049f5de322ca2bdd96572c56a4ad00ca
Maria Elena Tripoli <meatripoli@gmail.com>
2020-04-16 16:27:54 -0500

    removed reference to old file in indexOld.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "33d6b6950ee3b9a5fed8";
+/******/ 	var hotCurrentHash = "500ebe7270bf6d2d2629";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/indexOld.js
+++ pages/indexOld.js
@@ -1,5 +1,4 @@
 import NavBar from  './navBar';
-import Background from '../components/background';
 
 export default function Home() {
     return (<> 
